77bba11fd47fd73bc6e184a2c3d841923073b260
Alexander Mancevice <amancevice@cargometrics.com>
2017-12-19 15:38:08 -0500

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='fest',
-      version='0.0.2a1',
+      version='0.1.0',
       author='amancevice',
       author_email='smallweirdnum@gmail.com',
       packages=['fest'],
