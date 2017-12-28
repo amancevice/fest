f965b82a189c8fc8a67a42e5a57b12231420585e
Alexander Mancevice <amancevice@cargometrics.com>
2017-12-28 13:31:54 -0500

    env

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import textwrap
 from setuptools import setup
 
+CLI = ['click >= 6.7.0, < 6.8', 'ipython']
+GOOGLE = ['google >= 1.9.3, < 2', 'google-api-python-client >= 1.6.4, < 2']
+TRIBE = ['python-wordpress-xmlrpc >= 2.3.0, < 2.4']
+ALL = CLI + GOOGLE + TRIBE
+
 
 setup(name='fest',
-      version='0.1.6',
+      version='0.2.0',
       author='amancevice',
       author_email='smallweirdnum@gmail.com',
       packages=['fest'],
@@ -12,8 +17,9 @@ setup(name='fest',
       long_description=textwrap.dedent(
           '''See GitHub_ for documentation.
           .. _GitHub: https://github.com/amancevice/fest'''),
-      install_requires=['click >= 6.7.0, < 6.8',
-                        'facebook-sdk >= 2.0.0, < 3',
-                        'google >= 1.9.3, < 2',
-                        'google-api-python-client >= 1.6.4, < 2'],
+      install_requires=['facebook-sdk >= 2.0.0, < 3'],
+      extras_require={'all': ALL,
+                      'cli': CLI,
+                      'google': GOOGLE,
+                      'tribe': TRIBE},
       entry_points={'console_scripts': ['fest=fest.main:fest']})
