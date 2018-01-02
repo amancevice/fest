216f08cff46bd97ac769906ce0ad463064d000c4
Alexander Mancevice <amancevice@cargometrics.com>
2018-01-02 14:15:44 -0500

    0.2.0b1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ALL = CLI + GOOGLE + WORDPRESS
 
 
 setup(name='fest',
-      version='0.2.0b0',
+      version='0.2.0b1',
       author='amancevice',
       author_email='smallweirdnum@gmail.com',
       packages=['fest'],
