942b0d1d30b8f6218aaf9cb461117df45d8d4297
Alexander Mancevice <amancevice@cargometrics.com>
2018-12-11 14:34:19 -0500

    Simplify Pipfile/setup

--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 from setuptools import setup
 
-GOOGLE = [
-    'google >= 1.9.3',
-    'google-auth >= 1.5.1',
-    'google-api-python-client >= 1.6.4']
-WORDPRESS = ['python-wordpress-xmlrpc >= 2.3.0']
-ALL = GOOGLE + WORDPRESS
-
 setup(
     author='amancevice',
     author_email='smallweirdnum@gmail.com',
-    description='Sync Facebook events to other services',
-    extras_require={
-        'all': ALL,
-        'google': GOOGLE,
-        'wordpress': WORDPRESS,
-    },
+    description='Sync facebook events to Google Calendar',
     install_requires=[
         'facebook-sdk >= 3.0.0',
-        'python-dateutil >= 2.6.1',
+        'google-api-python-client >= 1.6.4',
         'pytz >= 2018.3',
         'requests >= 2.20.0',
     ],
