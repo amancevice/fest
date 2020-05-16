1588df1744ff41dbed72ffda76cfe6e46a740a11
Alexander Mancevice <alexander.mancevice@gmail.com>
2020-05-16 09:31:53 -0400

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(
     author='amancevice',
     author_email='smallweirdnum@gmail.com',
@@ -10,8 +13,11 @@ setup(
         'google-api-python-client >= 1.6.4',
         'requests >= 2.20',
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     name='fest',
     packages=find_packages(exclude=['tests']),
+    python_requires='>=3.6',
     setup_requires=['setuptools_scm'],
     url='https://github.com/amancevice/fest',
     use_scm_version=True,
