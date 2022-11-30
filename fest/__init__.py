cf5461323df9ab6993fa9a184492190dfa8aa278
Alexander Mancevice <alexander.mancevice@hey.com>
2022-11-29 19:14:29 -0500

    migrate to pyproject.toml

--- fest/__init__.py
+++ fest/__init__.py
@@ -1,12 +1,7 @@
 """
 Facebook Events Sync
 """
-import pkg_resources
-
 from fest.facebook import FacebookPage  # noqa: F401
 from fest.google import GoogleCalendar  # noqa: F401
 
-try:
-    __version__ = pkg_resources.get_distribution(__package__).version
-except pkg_resources.DistributionNotFound:  # pragma: no cover
-    __version__ = None  # pragma: no cover
+__version__ = "5.1.2"
